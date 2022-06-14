f720282996f2f7b4727abec922e0a2ec978c4800
Jessica Serafini <jpitts_kiheihs@yahoo.com>
2022-06-14 14:14:29 -0400

    commit tree 0f151bcf8a8f235b5f13753b0493e06e5a192bc5

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction(callback) {
+    callback() 
+
+}
+
+function returnsANamedFunction() {
+    return function index(){
+
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {
+
+    }
+}
+
